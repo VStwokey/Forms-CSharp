59db7f9cac744977556e08256c94b8fecf885490
VINICIUS SANTANA DE LIMA PAZ <vinicius.slpaz@senacsp.edu.br>
2023-06-27 14:31:59 -0300

    Formulário CSharp

---
+++ Sistema.view/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sistema.Entidades;
+using Sistema.Model;
+using Sistema.View;
+
+namespace Sistema.view
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            frmCadUsuarios form = new frmCadUsuarios();
+
+            form.Show(); 
+        }
+    }
+}
